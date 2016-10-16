7d6cddb69dabb2b372a7f7abbdb675a16a8fb4dd
kanulp <karangajjaro6@gmail.com>
2016-10-16 23:19:41 +0530

    Update and rename certi.java to Certi.java

--- JavaOep/Certi.java
+++ JavaOep/Certi.java
@@ -1,18 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 import java.awt.*;
 import java.io.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.event.*;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -32,11 +28,16 @@ public class Certi  extends JFrame{
 		this.s3=s3;
 
 	JFrame frame = new JFrame();
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("C:\\Users\\Shreeji123\\Desktop\\ss.jpg")))));
+    //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	frame.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("ss.jpg")))));
     JLabel label1 = new JLabel(s1);
 	JLabel label2 = new JLabel(s2);
 	JLabel label3 = new JLabel(s3);
+	label1.setFont(new Font("Serif", Font.BOLD, 17));
+		label2.setFont(new Font("Serif", Font.BOLD, 17));
+	label3.setFont(new Font("Serif", Font.BOLD, 17));
+
     frame.add(label1);
 	frame.add(label2);
 	frame.add(label3);
@@ -47,21 +48,20 @@ public class Certi  extends JFrame{
 	    Dimension size2 = label2.getPreferredSize();
     Dimension size3 = label3.getPreferredSize();
 
-    label1.setBounds(210, 170, size.width, size.height);
-	label2.setBounds(210, 190, size2.width, size2.height);
-	label3.setBounds(210, 220, size3.width, size3.height);
-    frame.setSize(500, 400);
+    label1.setBounds(230, 265, size.width, size.height);
+	label2.setBounds(450, 295, size2.width, size2.height);
+	label3.setBounds(150, 295, size3.width, size3.height);
+    frame.setSize(650, 520);
     frame.setVisible(true);
 
       Dimension size11 = frame.getSize();
-      //BufferedImage image = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_RGB);
       BufferedImage image = (BufferedImage)frame.createImage(size11.width, size11.height);
       Graphics g = image.getGraphics();
       frame.paint(g);
       g.dispose();
       try
       {
-        ImageIO.write(image, "jpg", new File("Certi.jpg"));
+        ImageIO.write(image, "jpg", new File(s1+".jpg"));
       }
       catch (IOException e)
       {
@@ -71,7 +71,14 @@ public class Certi  extends JFrame{
   
 public static void main(String args[]) throws IOException{
 	
-		Certi c = new Certi("hi","hi2","hi3");
-    
+		Certi c = new Certi("Karan Gajjar","3rd year","Relay-Coding");
+c.addWindowListener(new WindowAdapter()
+{ 
+public void windowClosing(WindowEvent e)
+{ 
+System.exit(0); 
+} 
+});
+
     }
 }
